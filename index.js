eac8b6ee12133434b59b0650fe88c04b6c3bf35b
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2014-05-20 11:44:38 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+
+function parse (url) {
+	return parse_url.exec(url);	
+}
+
+module.exports = parse;
+
